ddea2c0390fd0c6a90e37c416e0cb4c0d2012de0
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2025-01-26 14:06:48 +0000

    edit upper border

--- binary_search.py
+++ binary_search.py
@@ -1,8 +1,8 @@
 def binary_search(arr, x):
     low = 0
     high = len(arr) - 1
-    mid = 0
     num_of_iteration = 1
+    upper_bound = arr[-1]
  
     while low <= high:
  
@@ -15,20 +15,17 @@ def binary_search(arr, x):
  
         # if x is less than the value in the middle of the list, ignore the right half
         elif arr[mid] > x:
+            upper_bound = mid
             high = mid - 1
             num_of_iteration += 1
  
         # return number of needed iterations and index of element
         else:
             return (num_of_iteration, mid)
-        
-        # if low is greater or equal than high return number of iterations and index of upper limit
-        if low >= high:
-            return (num_of_iteration, low)
  
-    # if the item is not found
-    return "Element is not present in array"
+    # if the item is not found return upper bound
+    return (num_of_iteration, upper_bound)
 
 arr = [2.2, 2.5, 3, 3.9, 4.7, 11, 13, 15, 40.5, 40.8, 40.9]
-x = 2.5
+x = 2.3
 print(binary_search(arr, x))
